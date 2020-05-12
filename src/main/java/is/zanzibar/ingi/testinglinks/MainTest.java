96363e65418aa8f593d99011c959898d4d6f0b9b
linda <lindaecutbildning@gmail.com>
2020-05-12 16:23:57 +0200

    Morphbank url-address changed Jsoup-code snippet changed (shortened) Timeout set to 0 (change this?)

--- src/main/java/is/zanzibar/ingi/testinglinks/MainTest.java
+++ src/main/java/is/zanzibar/ingi/testinglinks/MainTest.java
@@ -32,7 +32,7 @@ public class MainTest {
 
     public static void main(String[] args) throws MalformedURLException {
 
-        MainTest testing = new MainTest();
+         MainTest testing = new MainTest();
         // pingar alla 'siter' på naturforskaren 
         List<String> fetchAllPingSites = testing.fetchAllPingSites();
 
@@ -45,21 +45,39 @@ public class MainTest {
         String site = testing.fetchSingleSearchSite("site.mediafiles.google");
         
         final String scientificName = "Gonepteryx rhamni"; // citronfjäril
+        final String corvus = "Corvus"; //kråksläkte
+        final String corax = "Corvus corax"; //korp
+        final String korp = "103036"; //Artportalen, taxon-test
+        
         ExternalLink linkGoogle = testing.checkGoogle(site, scientificName);
         System.out.println("\n\n Google search for '"+scientificName +"' images was "+linkGoogle.isLinkOk());
+        System.out.println("\n\n");
 
+         String site2 = testing.fetchSingleSearchSite("site.mediafiles.morphbank");
+         ExternalLink linkMorph = testing.checkMorphbank(site2, corvus);
+         System.out.println("\nMorphbank search for '" + corvus + "' images was " + linkMorph.isLinkOk());
+
+         String site3 = testing.fetchSingleSearchSite("site.mediafiles.flickr");
+         ExternalLink linkFlickr = testing.checkFlickr(site3, corax);
+         System.out.println("\nFlickr search for '" + corax + "' images was " + linkFlickr.isLinkOk());
+        
+         String site4 = testing.fetchSingleSearchSite("site.mediafiles.artportalen");
+         ExternalLink linkArtportalen = testing.checkArtportalen(site4, korp);
+         System.out.println("\nArtportalen search for '" + korp + "' images was " + linkArtportalen.isLinkOk());
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////
     private List fetchAllPingSites() {
         List<String> list = ReadPingSitesProperties.getAllValues();
         return list;
     }
     
+    
     private String fetchSingleSearchSite(String site) {
         String value = ReadSearchSitesProperties.getProperty(site);
         return value;
     }
-
+    
     private ExternalLink pinging(String site) throws MalformedURLException {
         ExternalLink link = new ExternalLink(new URL(site));
         boolean isPingable = this.genericPing(site);
@@ -75,7 +93,6 @@ public class MainTest {
 
     protected boolean genericPing(String website) {
         boolean isOK = false;
-
         try {
             HttpURLConnection.setFollowRedirects(false);
             HttpURLConnection con = (HttpURLConnection) new URL(website).openConnection();
@@ -84,6 +101,9 @@ public class MainTest {
             con.setConnectTimeout(CONNECT_TIMEOUT);
             con.setReadTimeout(CONNECT_TIMEOUT);
             final int responseCode = con.getResponseCode();
+            //////Linda/
+            System.out.println("Response code for " + website + " is: " + responseCode);
+            ///////////
 
             isOK = (responseCode == HttpURLConnection.HTTP_OK)
                     || (con.getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM)
@@ -104,7 +124,6 @@ public class MainTest {
             extendedWebSite = site + URLEncoder.encode(scientificName, ENCODING);
         } catch (UnsupportedEncodingException ex) {
             logger.log(Level.SEVERE, "Problem to close stream", ex);
-
         }
 
         try {
@@ -126,11 +145,115 @@ public class MainTest {
 
         return external;
     }
+    
+    /////////////////////////////////////////////////////////////////////
+   protected ExternalLink checkMorphbank (String site, String corvus) throws MalformedURLException {
+       ExternalLink external = new ExternalLink(new URL(site));
+       
+       try {
+            String url = (site + URLEncoder.encode(corvus, ENCODING));
+            URL u = new URL(url);
+            external.setLinkAddress(u);
 
+                
+                Elements elements = getLinkNodeJsoup(url, "#row1");
+
+                  if (elements.isEmpty()){
+                    external.setLinkOk(Boolean.FALSE);
+                } else {
+                    external.setLinkOk(Boolean.TRUE);
+                }
+
+        } catch (UnsupportedEncodingException ex) {
+            logger.log(Level.INFO, "Morphbank", ex.getMessage());
+            System.out.println("1: " + ex.getMessage());
+        } catch (MalformedURLException ex) {
+            logger.log(Level.INFO, "Morphbank", ex.getMessage());
+            System.out.println("2: " + ex.getMessage());
+        } catch (Exception ex) {
+           logger.log(Level.INFO, "Morphbank", ex.getMessage());
+            System.out.println("3: " + ex.getMessage());
+        }
+        return external;
+    } 
+
+    protected ExternalLink checkFlickr (String site, String corax) throws MalformedURLException {
+        ExternalLink external = new ExternalLink(new URL(site));
+
+        String extendedWebSite = "";
+        try {
+            extendedWebSite = site + URLEncoder.encode(corax, ENCODING);
+        } catch (UnsupportedEncodingException ex) {
+            logger.log(Level.SEVERE, "Problem to close stream", ex);
+        }
+
+        try {
+            URL url = new URL(extendedWebSite);
+            external.setLinkAddress(url);
+            Elements elements = getLinkNodeJsoup(extendedWebSite, ".interaction-view");
+            if (!elements.isEmpty()) {            
+                external.setLinkOk(Boolean.TRUE);
+            } else {
+                external.setLinkOk(Boolean.FALSE);
+            }
+              
+        } catch (UnsupportedEncodingException ex) {
+            logger.log(Level.INFO, "Flickr", ex.getMessage());
+        } catch (MalformedURLException ex) {
+            logger.log(Level.INFO, "Flickr", ex.getMessage());
+        } catch (Exception ex) {
+            logger.log(Level.INFO, "Flickr", ex.getMessage());
+        }
+
+        return external;
+    }
+    
+    protected ExternalLink checkArtportalen (String site, String korp) throws MalformedURLException {
+        ExternalLink external = new ExternalLink(new URL(site));
+
+        String extendedWebSite = "";
+        try {
+            extendedWebSite = site + URLEncoder.encode(korp, ENCODING);
+        } catch (UnsupportedEncodingException ex) {
+            logger.log(Level.SEVERE, "Problem to close stream", ex);
+        }
+
+        try {
+            URL url = new URL(extendedWebSite);
+            external.setLinkAddress(url);
+            Elements elements = getLinkNodeJsoup(extendedWebSite, "li.imageitem");
+            
+            if (!elements.isEmpty()) {
+                external.setLinkOk(Boolean.TRUE);
+            } else {
+                external.setLinkOk(Boolean.FALSE);
+            }
+            
+        } catch (UnsupportedEncodingException ex) {
+            logger.log(Level.INFO, "Artportalen", ex.getMessage());
+        } catch (MalformedURLException ex) {
+            logger.log(Level.INFO, "Artportalen", ex.getMessage());
+        } catch (Exception ex) {
+            logger.log(Level.INFO, "Artportalen", ex.getMessage());
+        }
+
+        return external;
+    }
+    
+    /*
+        site.mediafiles.morphbank=http://www.morphbank.net/MyManager/?keywords=
+        site.mediafiles.flickr=https://www.flickr.com/search/?text=
+        # obs: bilder för artportalen, vi måste hämta siffran -> https://www.artportalen.se/media/Taxon/103036
+        site.mediafiles.artportalen=https://www.artportalen.se/media/Taxon/
+    */
+
+    
+    /////////////////////////ADNILDNILNILILL////////////////////////////////////////////
     // used by checking: Google and xx and yyy and zz 
     private Elements getLinkNodeJsoup(String url, String selector) throws Exception {
         int timeOut = 10 * 1000;
-        Document document = Jsoup.connect(url).userAgent("Mozilla").timeout(timeOut).get();
+        /////Linda///
+        Document document = Jsoup.connect(url).userAgent("Mozilla").timeout(0).get();
         Elements elements = document.select(selector);
 
         return elements;
